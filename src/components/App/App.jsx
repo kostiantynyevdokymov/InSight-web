8dbccd21525fe8e09641001a1ebc3458b1c42d80
ihorlihus <ligusigor@ukr.net>
2022-12-18 14:55:26 +0200

    homePage web

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,10 @@
+import { Routes, Route } from 'react-router-dom';
+import { Home } from '../pages/Home';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React template
-    </div>
+    <Routes>
+      <Route path="/InSight-web" element={<Home />} />
+    </Routes>
   );
 };
