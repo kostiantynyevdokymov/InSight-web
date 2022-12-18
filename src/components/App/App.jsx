9ceb3eb17dfac6f3795a52c143ee7c994c353702
Kostiantyn <cashalot80@ukr.net>
2022-12-18 18:32:00 +0000

    add image index.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,9 @@
+import Loader from 'components/Loader/Loader';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React template
+    <div>
+      <Loader />
     </div>
   );
 };
