32b4c8001723b514b4838ef3ba2e7b208d895da9
AegisVP <vlad@pysarenko.com>
2022-12-19 20:43:29 +0100

    fix selector naming

--- src/redux/user/userSelectors.js
+++ src/redux/user/userSelectors.js
@@ -1,5 +1,5 @@
 export const selectUserData = store => store.params;
-export const selectAuth = ({ name, email, token }) => ({ name, email, token });
-export const selectToken = store => store.token;
-export const selectIsLoading = store => store.isLoading;
-export const selectError = store => store.error;
+export const selectUserAuth = ({ name, email, token }) => ({ name, email, token });
+export const selectUserToken = store => store.token;
+export const selectUserIsLoading = store => store.isLoading;
+export const selectUserError = store => store.error;
