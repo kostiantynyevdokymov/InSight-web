6483a4666ee7d48a6fc016cdcdd082ef86026a2a
Kostiantyn <cashalot80@ukr.net>
2022-12-19 13:56:23 +0000

    update userSelector

--- src/redux/user/userSelectors.js
+++ src/redux/user/userSelectors.js
@@ -3,3 +3,5 @@ export const selectAuth = ({ name, email, token }) => ({ name, email, token });
 export const selectToken = store => store.token;
 export const selectIsLoading = store => store.isLoading;
 export const selectError = store => store.error;
+
+export const selectLoaderShow = store =>
