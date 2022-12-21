f207530ec8cc304db90cf86d3432fba2712d5d71
ihorlihus <ligusigor@ukr.net>
2022-12-22 01:42:12 +0200

    add 2div and adaptive

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,11 @@
+import { FormDialy, Header } from './HeaderFofm';
 import { Wrapper, Layer, Leaves, Banana, Strawberry } from './Wrapper';
 
 export const Home = () => {
   return (
     <Wrapper>
+      <Header></Header>
+      <FormDialy></FormDialy>
       <Strawberry></Strawberry>
       <Banana></Banana>
       <Leaves></Leaves>
--- src/components/pages/Wrapper.js
+++ src/components/pages/Wrapper.js
@@ -8,23 +8,23 @@ import strawberry from '../../images/orig/Strawberry.png';
 export const Strawberry = () => {
   const StrawberryStyled = styled.img`
     position: absolute;
-    z-index: 4;
+    z-index: -2;
     //in figma shedow 10px 20px 50px ??
     filter: drop-shadow(10px 20px 10px rgba(0, 0, 0, 0.3));
     transform: matrix(-1, 0, 0, 1, 0, 0);
 
     @media (max-width: 1280px) {
-      width: 208px;
-      height: 203px;
-      left: 464px;
-      top: 583px;
+      width: calc(100% / 3.69);
+      height: calc(100% / 5.04);
+      left: calc(100% / 1.66);
+      top: calc(100% / 1.76);
     }
 
     @media (min-width: 1280px) {
-      width: 286px;
-      height: 279px;
-      left: 944px;
-      top: 506px;
+      width: calc(100% / 4.48);
+      height: calc(100% / 3.05);
+      left: calc(100% / 1.36);
+      top: calc(100% / 1.68);
     }
   `;
   return <StrawberryStyled src={strawberry} alt="strawberry" />;
@@ -33,20 +33,20 @@ export const Strawberry = () => {
 export const Banana = () => {
   const BananaStyled = styled.img`
     position: absolute;
-    z-index: 3;
+    z-index: -3;
 
     @media (max-width: 1280px) {
-      width: 740px;
-      height: 527px;
-      left: 247px;
-      top: 596px;
+      width: calc(100% / 1.04);
+      height: calc(100% / 1.94);
+      left: calc(100% / 3.11);
+      top: calc(100% / 1.71);
     }
 
     @media (min-width: 1280px) {
-      width: 773px;
-      height: 552px;
-      left: 782px;
-      top: -102px;
+      width: calc(100% / 1.66);
+      height: calc(100% / 1.54);
+      left: calc(100% / 1.64);
+      top: calc(- (100% / 1.66));
     }
   `;
   return <BananaStyled src={banana} alt="banana" />;
@@ -55,20 +55,20 @@ export const Banana = () => {
 export const Layer = () => {
   const LayerStyled = styled.img`
     position: absolute;
-    z-index: 1;
+    z-index: -4;
 
     @media (max-width: 1280px) {
-      width: 553px;
-      height: 750px;
-      left: 247px;
-      top: 461px;
+      width: calc(100% / 1.39);
+      height: calc(100% / 1.37);
+      left: calc(100% / 3.11);
+      top: calc(100% / 1.64);
     }
 
     @media (min-width: 1280px) {
-      width: 602.01px;
-      height: 816px;
-      left: 677.99px;
-      top: 34px;
+      width: calc(100% / 2.12);
+      height: calc(100% / 1.04);
+      left: calc(100% / 1.89);
+      top: calc(100% / 25);
     }
   `;
   return <LayerStyled src={layer} alt="layer" />;
@@ -77,22 +77,24 @@ export const Layer = () => {
 export const Leaves = () => {
   const LeavesStyled = styled.img`
     position: absolute;
-    z-index: 2;
+    z-index: -1;
+    animation: anim 10s infinite linear;
+    animation-delay: 2.5s;
 
     @media (max-width: 1280px) {
-      width: 531px;
-      height: 602px;
-      top: 463px; //in figma 493 ??
-      left: 40px;
+      width: calc(100% / 1.45);
+      height: calc(100% / 1.7);
+      top: calc(100% / 2.08);
+      left: calc(100% / 25.6);
 
       transform: rotate(90deg);
     }
 
     @media (min-width: 1280px) {
-      width: 746px;
-      height: 846px;
-      left: 323px;
-      top: -26px;
+      width: calc(100% / 1.71);
+      height: calc(100% / 1.005);
+      left: calc(100% / 3.96);
+      top: calc(- (100% / 32.7));
     }
   `;
   return <LeavesStyled src={leaves} alt="leaves" />;
@@ -102,19 +104,6 @@ export const Wrapper = styled.div`
   display: flex;
   position: relative;
   overflow: hidden;
-  /* border: 1px solid green;
-  margin: 50px; */
-  @media (max-width: 1200px) {
-    width: 768px;
-    height: 1024px;
-  }
-
-  @media (min-width: 1280px) {
-    width: 1280px;
-    height: 850px;
-    background-color: ${p => p.theme.colors.background.pageBackground};
-
-    margin-top: 20px;
-    margin-left: 20px;
-  }
+  width: 100%;
+  height: 100vh;
 `;
