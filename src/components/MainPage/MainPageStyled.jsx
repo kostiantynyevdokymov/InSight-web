b5db200eacfe22b63028297cd6f7e067c97530fd
ihorlihus <ligusigor@ukr.net>
2022-12-23 01:44:52 +0200

    fix img components

--- src/components/MainPage/MainPageStyled.jsx
+++ src/components/MainPage/MainPageStyled.jsx
@@ -7,88 +7,18 @@ import banana from '../../images/bcg-pages/banana.png';
 import strawberry from '../../images/bcg-pages/strawbeery.png';
 
 export const Strawberry = () => {
-  const StrawberryStyled = styled.img`
-    position: absolute;
-    z-index: -2;
-    //in figma shedow 10px 20px 50px ??
-    filter: drop-shadow(10px 20px 10px rgba(0, 0, 0, 0.3));
-    transform: matrix(-1, 0, 0, 1, 0, 0);
-    @media (max-width: 1280px) {
-      width: calc(100% / 3.69);
-      height: calc(100% / 5.04);
-      left: calc(100% / 1.66);
-      top: calc(100% / 1.76);
-    }
-    @media (min-width: 1280px) {
-      width: calc(100% / 4.48);
-      height: calc(100% / 3.05);
-      left: calc(100% / 1.36);
-      top: calc(100% / 1.68);
-    }
-  `;
   return <StrawberryStyled src={strawberry} alt="strawberry" />;
 };
 
 export const Banana = () => {
-  const BananaStyled = styled.img`
-    position: absolute;
-    z-index: -3;
-    @media (max-width: 1280px) {
-      width: calc(100% / 1.04);
-      height: calc(100% / 1.94);
-      left: calc(100% / 3.11);
-      top: calc(100% / 1.71);
-    }
-    @media (min-width: 1280px) {
-      width: calc(100% / 1.66);
-      height: calc(100% / 1.54);
-      left: calc(100% / 1.64);
-      top: calc(- (100% / 1.66));
-    }
-  `;
   return <BananaStyled src={banana} alt="banana" />;
 };
 
 export const Layer = () => {
-  const LayerStyled = styled.img`
-    position: absolute;
-    z-index: -4;
-    @media (max-width: 1280px) {
-      width: calc(100% / 1.39);
-      height: calc(100% / 1.37);
-      left: calc(100% / 3.11);
-      top: calc(100% / 1.64);
-    }
-    @media (min-width: 1280px) {
-      width: calc(100% / 2.12);
-      height: calc(100% / 1.04);
-      left: calc(100% / 1.89);
-      top: calc(100% / 25);
-    }
-  `;
   return <LayerStyled src={layer} alt="layer" />;
 };
 
 export const Leaves = () => {
-  const LeavesStyled = styled.img`
-    position: absolute;
-    z-index: -1;
-    animation: anim 10s infinite linear;
-    animation-delay: 2.5s;
-    @media (max-width: 1280px) {
-      width: calc(100% / 1.45);
-      height: calc(100% / 1.7);
-      top: calc(100% / 2.08);
-      left: calc(100% / 25.6);
-      transform: rotate(90deg);
-    }
-    @media (min-width: 1280px) {
-      width: calc(100% / 1.71);
-      height: calc(100% / 1.005);
-      left: calc(100% / 3.96);
-      top: calc(- (100% / 32.7));
-    }
-  `;
   return <LeavesStyled src={leaves} alt="leaves" />;
 };
 
@@ -98,4 +28,78 @@ export const Wrapper = styled.div`
   overflow: hidden;
   width: 100%;
   height: calc(100vh - (${theme.spacing[5]}px + 40px));
+`;
+
+const StrawberryStyled = styled.img`
+  position: absolute;
+  z-index: -2;
+  //in figma shedow 10px 20px 50px ??
+  filter: drop-shadow(10px 20px 10px rgba(0, 0, 0, 0.3));
+  transform: matrix(-1, 0, 0, 1, 0, 0);
+  @media (max-width: 1280px) {
+    width: calc(100% / 3.69);
+    height: calc(100% / 5.04);
+    left: calc(100% / 1.66);
+    top: calc(100% / 1.76);
+  }
+  @media (min-width: 1280px) {
+    width: calc(100% / 4.48);
+    height: calc(100% / 3.05);
+    left: calc(100% / 1.36);
+    top: calc(100% / 1.68);
+  }
+`;
+
+const BananaStyled = styled.img`
+  position: absolute;
+  z-index: -3;
+  @media (max-width: 1280px) {
+    width: calc(100% / 1.04);
+    height: calc(100% / 1.94);
+    left: calc(100% / 3.11);
+    top: calc(100% / 1.71);
+  }
+  @media (min-width: 1280px) {
+    width: calc(100% / 1.66);
+    height: calc(100% / 1.54);
+    left: calc(100% / 1.64);
+    top: calc(- (100% / 1.66));
+  }
+`;
+
+const LayerStyled = styled.img`
+  position: absolute;
+  z-index: -4;
+  @media (max-width: 1280px) {
+    width: calc(100% / 1.39);
+    height: calc(100% / 1.37);
+    left: calc(100% / 3.11);
+    top: calc(100% / 1.64);
+  }
+  @media (min-width: 1280px) {
+    width: calc(100% / 2.12);
+    height: calc(100% / 1.04);
+    left: calc(100% / 1.89);
+    top: calc(100% / 25);
+  }
+`;
+
+const LeavesStyled = styled.img`
+  position: absolute;
+  z-index: -1;
+  animation: anim 10s infinite linear;
+  animation-delay: 2.5s;
+  @media (max-width: 1280px) {
+    width: calc(100% / 1.45);
+    height: calc(100% / 1.7);
+    top: calc(100% / 2.08);
+    left: calc(100% / 25.6);
+    transform: rotate(90deg);
+  }
+  @media (min-width: 1280px) {
+    width: calc(100% / 1.71);
+    height: calc(100% / 1.005);
+    left: calc(100% / 3.96);
+    top: calc(- (100% / 32.7));
+  }
 `;
