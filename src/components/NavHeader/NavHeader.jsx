690f07d8de39280518cfaa5ef63b3dc589a6b481
Polosmakr <polosmakrr@gmail.com>
2022-12-23 17:42:01 +0200

    logout
    змінив кнопку logout

--- src/components/NavHeader/NavHeader.jsx
+++ src/components/NavHeader/NavHeader.jsx
@@ -1,14 +1,22 @@
 import { useAuth } from 'hooks/useAuth';
 import { Exit, Name, NavHeaderContainer } from './NavHeader.styled';
+import { useDispatch } from 'react-redux';
+import { logoutUser } from 'redux/user/userOperations';
 
 export const NavHeader = () => {
+  const dispatch = useDispatch();
   const { name } = useAuth();
+
   return (
     <>
       <NavHeaderContainer>
         <Name type="button">Hello {name}</Name>
-        <Exit>
-          <a href="./logout">Exit</a>
+        <Exit
+          onClick={() => {
+            dispatch(logoutUser());
+          }}
+        >
+          Exit
         </Exit>
       </NavHeaderContainer>
     </>
