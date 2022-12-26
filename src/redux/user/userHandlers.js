0176ba45066a7ea938e01a4c3d9655399adcf3f8
AegisVP <vlad@pysarenko.com>
2022-12-26 17:03:11 +0100

    fix userHandlers

--- src/redux/user/userHandlers.js
+++ src/redux/user/userHandlers.js
@@ -2,11 +2,7 @@ export const handleLoginUser = (state, action) => (state = action.payload);
 export const handleRegisterUser = (state, action) => (state = action.payload);
 export const handleLogoutUser = state => (state = { ...state, token: null });
 export const handleRefreshUser = (state, action) => (state = action.payload);
-<<<<<<< Updated upstream
-export const handleSetParams = (state, action) => { state.params = action.payload };
-=======
 export const handleSetParams = (state, action) => {
   state.params = action.payload;
 };
->>>>>>> Stashed changes
 export const handleGoogleLogin = (state, action) => (state = action.payload);
