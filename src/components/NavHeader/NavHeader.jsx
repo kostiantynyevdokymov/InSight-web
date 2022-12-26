053c22f3c13cf43c28b7965d4fdcb537103bdeac
Polosmakr <polosmakrr@gmail.com>
2022-12-26 21:42:42 +0200

    connect with main

--- src/components/NavHeader/NavHeader.jsx
+++ src/components/NavHeader/NavHeader.jsx
@@ -11,8 +11,8 @@ export const NavHeader = () => {
   const navigate = useNavigate();
   const url = window.location.href.split('/').pop();
 
-  const dispatch = useDispatch();
-  const { name } = useSelector(selectUserAuth);
+  // const dispatch = useDispatch();
+  // const { name } = useSelector(selectUserAuth);
 
   const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
   const isTablet = useMediaQuery({ query: '(min-width: 768px) and (max-width: 1279px)' });
