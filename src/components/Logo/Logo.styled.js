9e05e12ffce9fc43cee414b647078043f8685fd9
AegisVP <vlad@pysarenko.com>
2022-12-26 20:23:47 +0100

    rename js and jsx

--- src/components/Logo/Logo.styled.js
+++ src/components/Logo/Logo.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
-import { theme } from 'components/Common/Theme';
+
 export const Logo = styled.img`
-  margin-left: ${theme.spacing[3]}px;
+  margin-left: ${p => p.theme.spacing[3]}px;
   @media (min-width: 1280px) {
   }
 `;
