978bbde5a1b9b82798a292e7bd5cd3305e5a5092
AegisVP <vlad@pysarenko.com>
2022-12-27 09:26:11 +0100

    translate to english, add diet loading

--- src/components/CalculatorCalorieForm/CalculatorCalorieForm.jsx
+++ src/components/CalculatorCalorieForm/CalculatorCalorieForm.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useAuth } from 'hooks/useAuth';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
-import SC from './CalculatorCalorieForm.styled';
+import { useAuth } from 'hooks/useAuth';
 import { selectIsLoadingUser, selectUserParams } from 'redux/selectors';
 import { setParams } from 'redux/user/userSlice';
-import { useEffect } from 'react';
 import { fetchDiet, fetchUserDiet } from 'redux/diet/dietOperations';
-import { refreshUser } from 'redux/user/userOperations';
+import SC from './CalculatorCalorieForm.styled';
 
 function CalculatorCalorieForm({ modal }) {
   const dispatch = useDispatch();
@@ -24,34 +22,34 @@ function CalculatorCalorieForm({ modal }) {
   };
 
   const ErrorMessagesSchema = Yup.object().shape({
-    height: Yup.number('Значення має бути число')
-      .min(100, 'Вкажіть значення більше 100 см')
-      .max(260, 'Вкажіть значення менше 260 см')
-      .required("Обов'язкове поле"),
-    age: Yup.number('Значення має бути число')
-      .min(18, 'Вкажіть значення більше 18')
-      .max(100, 'Вкажіть значення менше 100')
-      .required("Обов'язкове поле"),
-    currentWeight: Yup.number('Значення має бути число')
-      .min(40, 'Мінімальна вага 40 кг')
-      .max(500, 'Максимальна вага 500 кг')
-      .required("Обов'язкове поле"),
-    desireWeight: Yup.number('Значення має бути число')
-      .min(40, 'Мінімальна вага 40 кг')
-      .max(500, 'Максимальна вага 500 кг')
-      .required("Обов'язкове поле")
+    height: Yup.number('This should be a number')
+      .min(100, 'Height should be more than 100 cm')
+      .max(260, 'Height should be less than 260 cm')
+      .required('This is a required field'),
+    age: Yup.number('This should be a number')
+      .min(18, 'Age should be 18')
+      .max(100, 'Age should be 100')
+      .required('This is a required field'),
+    currentWeight: Yup.number('This should be a number')
+      .min(40, 'Minimum possible weight is 40 kg')
+      .max(500, 'Maximum possible weight is 500 kg')
+      .required('This is a required field'),
+    desireWeight: Yup.number('This should be a number')
+      .min(40, 'Minimum possible weight is 40 kg')
+      .max(500, 'Maximum possible weight is 500 kg')
+      .required("This is a required field")
       .when('currentWeight', (currentWeight, schema) => {
         return schema.test({
           test: desireWeight => !!currentWeight && desireWeight < currentWeight,
-          message: 'Бажана вага повинна бути менше поточної',
+          message: 'Target weight should be less than your current weight',
         });
       }),
-    bloodType: Yup.number().required("Обов'язкове поле"),
+    bloodType: Yup.number().required("This is a required field"),
   });
 
   useEffect(() => {
-    if (isLoggedIn) dispatch(refreshUser());
-  }, [dispatch, isLoggedIn]);
+    if (isLoggedIn && !!userParams.height) dispatch(fetchUserDiet(userParams));
+  }, [dispatch, isLoggedIn, userParams]);
 
   return (
     <>
@@ -75,26 +73,46 @@ function CalculatorCalorieForm({ modal }) {
             <SC.InputWrapper>
               <SC.InputBlock>
                 <SC.Label>
-                  <SC.InputField placeholder=" " name="height" type="number" iserror={!!errors.height && !!touched.height ? 1 : 0} />
+                  <SC.InputField
+                    placeholder=" "
+                    name="height"
+                    type="number"
+                    iserror={!!errors.height && !!touched.height ? 1 : 0}
+                  />
                   <SC.LabelValue>Height *</SC.LabelValue>
                   <SC.Error component="div" name="height" />
                 </SC.Label>
 
                 <SC.Label>
-                  <SC.InputField placeholder=" " name="age" type="number" iserror={!!errors.age && touched.age ? 1 : 0} />
+                  <SC.InputField
+                    placeholder=" "
+                    name="age"
+                    type="number"
+                    iserror={!!errors.age && touched.age ? 1 : 0}
+                  />
                   <SC.LabelValue>Age *</SC.LabelValue>
                   <SC.Error component="div" name="age" />
                 </SC.Label>
 
                 <SC.Label>
-                  <SC.InputField placeholder=" " name="currentWeight" type="number" iserror={!!errors.currentWeight && touched.currentWeight ? 1 : 0} />
+                  <SC.InputField
+                    placeholder=" "
+                    name="currentWeight"
+                    type="number"
+                    iserror={!!errors.currentWeight && touched.currentWeight ? 1 : 0}
+                  />
                   <SC.LabelValue>Current weight *</SC.LabelValue>
                   <SC.Error component="div" name="currentWeight" />
                 </SC.Label>
               </SC.InputBlock>
               <SC.InputBlock>
                 <SC.Label>
-                  <SC.InputField placeholder=" " name="desireWeight" type="number" iserror={!!errors.desireWeight && touched.desireWeight ? 1 : 0} />
+                  <SC.InputField
+                    placeholder=" "
+                    name="desireWeight"
+                    type="number"
+                    iserror={!!errors.desireWeight && touched.desireWeight ? 1 : 0}
+                  />
                   <SC.LabelValue>Desired weight *</SC.LabelValue>
                   <SC.Error component="div" name="desireWeight" />
                 </SC.Label>
