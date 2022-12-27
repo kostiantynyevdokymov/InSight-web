56b4fa0adaa60fa288f02ef27f794bc5c7b08b69
Polosmakr <polosmakrr@gmail.com>
2022-12-27 13:15:34 +0200

    cursour pointer

--- src/components/BurgerMenu/BurgerMenu.styled.js
+++ src/components/BurgerMenu/BurgerMenu.styled.js
@@ -23,6 +23,7 @@ export const Diary = styled.button`
   font-size: ${p => p.theme.fontSizes.m};
   padding-bottom: ${p => p.theme.mp(3)};
   color: ${p => (p.active ? '#9B9FAA' : '#FFF')};
+  cursor: pointer;
 `;
 
 export const Calculator = styled(Diary)``;
